cfb1c335ae05e9c1ba98b294a8756d62145f19fd
Filip Krul <gvarph006@gmail.com>
2025-07-16 21:37:39 +0200

    Remove example endpoint from main

--- app/main.py
+++ app/main.py
@@ -5,11 +5,3 @@ from app.routers.crawl import router as crawl_router
 app = FastAPI()
 
 app.include_router(crawl_router)
-
-
-@app.get("/")
-def read_root() -> dict[str, str]:
-    return {"Hello": "World"}
-
-
-print("Started server")
