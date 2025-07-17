df6808a4c7174bcf5bfc322a58a413b1bdd87c81
Filip Krul <gvarph006@gmail.com>
2025-07-18 01:52:48 +0200

    Trigger crawl through endpoint

--- app/dependencies.py
+++ app/dependencies.py
@@ -2,20 +2,40 @@ from collections.abc import AsyncGenerator
 from typing import Annotated
 
 from fastapi import Depends, Request
+from httpx import AsyncClient
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.db import DBContext
 
 
-async def session(
+async def db_context(
     request: Request,
-) -> AsyncGenerator[AsyncSession]:
+) -> DBContext:
     """Provide a SQLAlchemy session for the request."""
     if not isinstance(request.app.state.db, DBContext):
-        msg = "DBContext not initialized"
+        msg = f"DBContext not initialized, {type(request.app.state.db)=}"
         raise RuntimeError(msg)  # noqa: TRY004
-    async with request.app.state.db.get_session() as session:
+    return request.app.state.db
+
+
+DBContextDep = Annotated[DBContext, Depends(db_context)]
+
+
+async def session(
+    db_context: DBContextDep,
+) -> AsyncGenerator[AsyncSession]:
+    """Provide a SQLAlchemy session for the request."""
+    async with db_context.get_session() as session:
         yield session
 
 
 SessionDep = Annotated[AsyncSession, Depends(session)]
+
+
+async def httpx_client() -> AsyncGenerator[AsyncClient]:
+    """Provide a HTTPX client for the request."""
+    async with AsyncClient() as client:
+        yield client
+
+
+HttpxClientDep = Annotated[AsyncClient, Depends(httpx_client)]
--- app/routers/crawl.py
+++ app/routers/crawl.py
@@ -1,13 +1,20 @@
 from fastapi import APIRouter
 
+from app.dependencies import DBContextDep, HttpxClientDep
+from app.load_data import PagesToCrawl, crawl_top_movies_and_actors
+
 router = APIRouter(prefix="/crawl", tags=["Crawl"])
 
 
 @router.post(
-    "/crawl",
+    "/load_movies_data",
     summary="Crawls ČSFD",
     status_code=204,
 )
-async def crawl() -> None:
+async def load_movies_data(
+    db_context: DBContextDep,
+    httpx_client: HttpxClientDep,
+    pages_to_crawl: PagesToCrawl = 1,
+) -> None:
     """Rebuilds our cache of the most popular movies and actors on ČSFD"""
-    raise NotImplementedError
+    await crawl_top_movies_and_actors(httpx_client, db_context, pages_to_crawl)
